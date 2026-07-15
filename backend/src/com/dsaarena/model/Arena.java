905055d77c022794abcccc9f57225bf2b1adab1f
mohd-ashraf24 <93ashraf717@gmail.com>
2026-07-15 17:22:57 +0530

    add winRating and lossRating fields

--- backend/src/com/dsaarena/model/Arena.java
+++ backend/src/com/dsaarena/model/Arena.java
@@ -9,7 +9,10 @@ public class Arena{
     private int minRating;
     private int readingTime;
     private int codingTime;
-    public Arena(int arenaId , String arenaName , String difficulty,int entryFee, int rewardCoins, int minRating, int readingTime, int codingTime)
+    private int winRating;
+    private int lossRating;
+
+    public Arena(int arenaId , String arenaName , String difficulty,int entryFee, int rewardCoins, int minRating, int readingTime, int codingTime , int winRating , int lossRating)
     {
         this.arenaId = arenaId;
         this.arenaName = arenaName;
@@ -19,6 +22,8 @@ public class Arena{
         this.minRating = minRating;
         this.readingTime = readingTime;
         this.codingTime = codingTime;
+        this.winRating = winRating;
+        this.lossRating = lossRating;
     }
     public int getArenaId(){
         return arenaId;
@@ -44,5 +49,13 @@ public class Arena{
     }
     public int getCodingTime(){
         return codingTime;
+    }
+
+    public int getWinRating() {
+        return winRating;
+    }
+
+    public int getLossRating() {
+        return lossRating;
     }
 }
