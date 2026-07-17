3d3d6798c211604ff36b902f813e1db632a6964f
mohd-ashraf24 <93ashraf717@gmail.com>
2026-07-17 16:08:00 +0530

    feat: implement SubmissionRepository with match-wise submission indexing

---
+++ backend/src/com/dsaarena/repository/submissionRepository.java
@@ -0,0 +1,65 @@
+package com.dsaarena.repository;
+
+import com.dsaarena.model.Submission;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class submissionRepository {
+    private Map<Long , Submission> submissions;
+    private Map<Long , List<Submission>> matchSubmissions;
+
+    public submissionRepository(){
+        submissions = new HashMap<>();
+        matchSubmissions = new HashMap<>();
+    }
+
+
+    //add submission
+    public void addSubmission(Submission submission){
+        if(submission == null){
+            return;
+        }
+        submissions.put(submission.getSubmissionId(), submission);
+        long matchId = submission.getMatchId();
+        if(!matchSubmissions.containsKey(matchId)){
+            matchSubmissions.put(matchId , new ArrayList<>());
+        }
+        matchSubmissions.get(matchId).add(submission);
+    }
+
+    // get submission by id
+    public Submission getSubmissionById(long submissionId){
+        return submissions.get(submissionId);
+    }
+
+    //get all submission
+    public List<Submission> getAllSubmission(){
+        return new ArrayList<>(submissions.values());
+    }
+
+    // get submission by match id
+    public List<Submission> getSubmissionsByMatchId(long matchId){
+        if(!matchSubmissions.containsKey(matchId)){
+            return new ArrayList<>();
+        }
+        return new ArrayList<>(matchSubmissions.get(matchId));
+    }
+
+    // remove submissions
+    public void removeSubmission(long submissionId){
+        Submission submission = submissions.get(submissionId);
+        if(submission == null) {
+            return;
+        }
+        long matchId = submission.getMatchId();
+        submissions.remove(submissionId);
+        List<Submission> submissionList = matchSubmissions.get(matchId);
+        submissionList.remove(submission);
+        if(submissionList.isEmpty()){
+            matchSubmissions.remove(matchId);
+        }
+    }
+}
