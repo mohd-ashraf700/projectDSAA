ec775a7f131f87e2155ecb836f0c62d2d98831f3
mohd-ashraf24 <93ashraf717@gmail.com>
2026-07-24 23:43:14 +0530

    implement getBestSubmission method

--- backend/src/com/dsaarena/service/ResultService.java
+++ backend/src/com/dsaarena/service/ResultService.java
@@ -1,13 +1,17 @@
 package com.dsaarena.service;
 
 import com.dsaarena.enums.MatchStatus;
+import com.dsaarena.enums.Verdict;
 import com.dsaarena.model.Arena;
 import com.dsaarena.model.Match;
+import com.dsaarena.model.Submission;
 import com.dsaarena.model.User;
 import com.dsaarena.repository.ArenaRepository;
 import com.dsaarena.repository.MatchRepository;
 import com.dsaarena.repository.SubmissionRepository;
 import com.dsaarena.repository.UserRepository;
+
+import java.util.List;
 
 public class ResultService {
     private MatchRepository matchRepository;
@@ -36,6 +40,9 @@ public class ResultService {
         }
         int arenaId = match.getArenaId();
         Arena arena = arenaRepository.getArenaById(arenaId);
+        if (arena == null) {
+            return;
+        }
         long player1Id = match.getPlayer1Id();
         long player2Id = match.getPlayer2Id();
 
@@ -44,6 +51,29 @@ public class ResultService {
         if(player1 == null || player2 == null){
             return;
         }
+        List<Submission> submissions =
+                submissionRepository.getSubmissionsByMatchId(matchId);
+        Submission player1Best = getBestSubmission(submissions , player1Id);
 
+        Submission player2Best = getBestSubmission(submissions , player2Id);
+
+    }
+    //get best submission
+    private Submission getBestSubmission(List<Submission> submissions,
+                                         long playerId){
+        Submission bestSubmission = null;
+        for(Submission submission : submissions){
+            if(submission.getPlayerId() != playerId){
+                continue;
+            }
+            if(submission.getVerdict() == Verdict.ACCEPTED){
+                return submission;
+            }
+            if(bestSubmission == null ||
+                        submission.getPassedTestCases() > bestSubmission.getPassedTestCases()){
+                    bestSubmission = submission;
+            }
+        }
+        return bestSubmission;
     }
 }
