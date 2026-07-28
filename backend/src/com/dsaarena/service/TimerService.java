ccc0289248fb301f0d559ccb031c6539fc1ba410
mohd-ashraf24 <93ashraf717@gmail.com>
2026-07-28 23:10:53 +0530

    implement fields and constructor of TimerService

--- backend/src/com/dsaarena/service/TimerService.java
+++ backend/src/com/dsaarena/service/TimerService.java
@@ -1,4 +1,21 @@
 package com.dsaarena.service;
 
+import com.dsaarena.model.Match;
+import com.dsaarena.repository.ArenaRepository;
+import com.dsaarena.repository.MatchRepository;
+
+import java.util.concurrent.ScheduledExecutorService;
+
 public class TimerService {
+    private final ScheduledExecutorService scheduler;
+    private final ResultService resultService;
+    private final MatchRepository matchRepository;
+    private final ArenaRepository arenaRepository;
+
+    public TimerService(ScheduledExecutorService scheduler, ResultService resultService, MatchRepository matchRepository, ArenaRepository arenaRepository) {
+        this.scheduler = scheduler;
+        this.resultService = resultService;
+        this.matchRepository = matchRepository;
+        this.arenaRepository = arenaRepository;
+    }
 }
