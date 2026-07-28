d49f6e0ba0cfd0d0e3b3eab33553de90a24662a6
mohd-ashraf24 <93ashraf717@gmail.com>
2026-07-29 00:51:33 +0530

    implement TimerService for v1

--- backend/src/com/dsaarena/service/TimerService.java
+++ backend/src/com/dsaarena/service/TimerService.java
@@ -1,10 +1,14 @@
 package com.dsaarena.service;
 
+import com.dsaarena.enums.MatchStatus;
+import com.dsaarena.model.Arena;
 import com.dsaarena.model.Match;
 import com.dsaarena.repository.ArenaRepository;
 import com.dsaarena.repository.MatchRepository;
 
+import java.time.LocalDateTime;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class TimerService {
     private final ScheduledExecutorService scheduler;
@@ -17,5 +21,31 @@ public class TimerService {
         this.resultService = resultService;
         this.matchRepository = matchRepository;
         this.arenaRepository = arenaRepository;
+    }
+    //start Match
+    public void startMatch(Match match) {
+        match.setStartTime(LocalDateTime.now());
+        match.setMatchStatus(MatchStatus.READING);
+        startReadingPhase(match);
+    }
+
+    private void startReadingPhase(Match match) {
+        Arena arena = arenaRepository.getArenaById((match.getArenaId()));
+        scheduler.schedule(() -> {
+            startCodingPhase(match);
+        },arena.getReadingTime(), TimeUnit.MINUTES);
+    }
+
+    private void startCodingPhase(Match match) {
+        match.setMatchStatus(MatchStatus.LIVE);
+        Arena arena = arenaRepository.getArenaById(match.getArenaId());
+        scheduler.schedule(() -> {
+            finishMatch(match);
+        } , arena.getCodingTime() , TimeUnit.MINUTES);
+    }
+
+    private void finishMatch(Match match) {
+        match.setEndTime(LocalDateTime.now());
+        resultService.processResult(match.getMatchId());
     }
 }
